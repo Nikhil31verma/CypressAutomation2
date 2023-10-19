90081fbbe8f8251dd645bf33e8d55b30b9d5a850
Nikhil Verma <nik98269@gmail.com>
2023-10-19 11:20:55 +0530

    Test101 first test suite

--- cypress/Integration/Examples/Test101.js
+++ cypress/Integration/Examples/Test101.js
@@ -1,2 +1,11 @@
-console.log("Hello test");
-console.log("123");
+/// <reference types="Cypress" />
+
+describe('My first test suite',function(){
+    it('My first test case',function(){
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/'); 
+        cy.get('.search-keyword').type('cu')
+        cy.wait(3000);
+        cy.get('.product:visible').should('have.length',2);
+    })
+})
+
