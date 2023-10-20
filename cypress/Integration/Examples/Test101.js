cba4d67ebc26e66002c52fc4517b4adcbbd18c5e
Nikhil Verma <nik98269@gmail.com>
2023-10-20 16:10:46 +0530

    ADD TO CART COMMIT

--- cypress/Integration/Examples/Test101.js
+++ cypress/Integration/Examples/Test101.js
@@ -2,10 +2,13 @@
 
 describe('My first test suite',function(){
     it('My first test case',function(){
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/'); 
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');  // cy is a predefined object used to get properties of cypress
         cy.get('.search-keyword').type('cu')
         cy.wait(3000);
         cy.get('.product:visible').should('have.length',2);
+        cy.get('.products').find('.product').should('have.length',2);
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
+        
     })
 })
 
