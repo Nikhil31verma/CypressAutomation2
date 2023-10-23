925f20f1bb0e14c06f1d5d02b981e9ef486c117e
Nikhil Verma <nik98269@gmail.com>
2023-10-23 17:34:27 +0530

    iterate array commit

--- cypress/Integration/Examples/Test102.js
+++ cypress/Integration/Examples/Test102.js
@@ -1,13 +1,11 @@
 /// <reference types="Cypress" />
 
-describe('My first test suite',function(){
-    it('My first test case',function(){
+describe('My Second test suite',function(){
+    it('My Second test case',function(){
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');  // cy is a predefined object used to get properties of cypress
         cy.get('.search-keyword').type('cu')
         cy.wait(2000);
-        cy.get('.product:visible').should('have.length',2);
-       //cy.get('.products').find('.product').should('have.length',2);
-        cy.get('.products').find('.product').eq(0).contains('ADD TO CART').click();
+      //  cy.get('.products').find('.product').eq(0).contains('ADD TO CART').click();
         cy.get('.products').find('.product').each(($el,index,$list) => {
 
            const textMat= $el.find('.product-name').text();
@@ -17,13 +15,10 @@ describe('My first test suite',function(){
            }
         } )
 
-       const logo= cy.get('.brand').then((logo) =>{
-        cy.log(logo.text());
        })
       // cy.log(logo.text());
      // cy.get('.brand').as('brand');  we can rename the .brand class to brand
      // cy.get('@brand').should('have.text','GREENKART');
         
     })
-})
 
